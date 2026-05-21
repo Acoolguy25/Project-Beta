a34bd19c36e59b194c6c3d23fe3d95ad62246a01
Acoolguy25 <ryan.bahary@gmail.com>
2026-05-21 13:52:20 -0500

    Small Username Mod

--- Assets/RyanAssets/Login/LoginManager.cs
+++ Assets/RyanAssets/Login/LoginManager.cs
@@ -19,13 +19,14 @@ namespace RyanAssets.Login {
             loginScreen.RefreshScreen();
             while (true){
                 PromptManager.PromptWait("Loading", "Connecting To Server", PromptId.NetworkLoginAwait);
-                (string errMsg, JObject res2) = await ServerNetwork.PostRequest("/api/v1/players/me");
+                (string errMsg, JObject res2) = await ServerNetwork.PostRequest("/api/players/v1/me");
                 PromptManager.PromptDelete(PromptId.NetworkLoginAwait);
                 if (errMsg != null){
                     await PromptManager.Instance.PromptLocalUser("Login Failed", errMsg, PromptId.LoginResponse, PromptManager.ButtonPreset_RetryOnly);
                 }
                 else{
                     // TODO: Do server manipulation here
+                    Debug.Log(res2);
                     break;
                 }
             }
