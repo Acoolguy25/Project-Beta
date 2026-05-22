bbf45a4fc0fe713c29fb920b733483f06e04e435
Acoolguy25 <ryan.bahary@gmail.com>
2026-05-22 11:20:47 -0500

    Before Universe In Full Swing

---
+++ Assets/Universes/UniverseCfg.cs
@@ -0,0 +1,11 @@
+namespace Universes {
+    public static class UniverseCfg {
+        public struct Universe {
+            public string id;
+            public string title;
+            public string description;
+            public string creator_playerid;
+            public string image_path;
+        };
+    }
+}
---
+++ Assets/Universes/UniverseUI.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Universes{
+    public class UniverseUI: MonoBehaviour {
+        void Start(){
+            
+        }
+    }
+}
