a88a945a48db0d1159a799fea2415278569e290c
Acoolguy25 <ryan.bahary@gmail.com>
2026-06-08 22:11:16 -0400

    Thumbnail + Client_init reversion

--- Assets/RyanAssets/Client/ClientCore/ClientInit.cs
+++ Assets/RyanAssets/Client/ClientCore/ClientInit.cs
@@ -4,7 +4,7 @@ namespace RyanAssets.Client.ClientCore {
     public class ClientInit: MonoBehaviour {
         [SerializeField]
         GameObject ClientPrefab;
-        void Start(){
+        void Awake(){
             if (ClientConnector.Instance == null)
                 Instantiate(ClientPrefab);
             Destroy(gameObject);
